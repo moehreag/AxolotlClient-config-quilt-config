baba45f5d822c56081d4e47b999ad4b8caadaedf
moehreag <moehreag@gmail.com>
2023-03-10 16:26:02 +0100

    organize the file structure better

---
+++ QuiltConfigUIProvider/src/main/java/io/github/moehreag/AxolotlClientConfig_QuiltConfig_UIProvider/AxolotlClientConfig_QuiltConfig_UIProvider.java
@@ -0,0 +1,17 @@
+package io.github.moehreag.AxolotlClientConfig_QuiltConfig_UIProvider;
+
+import io.github.axolotlclient.AxolotlClientConfig.AxolotlClientConfigManager;
+import io.github.moehreag.AxolotlClientConfig_Quilt_Config.QuiltConfigConverter;
+import org.quiltmc.config.api.Configs;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+public class AxolotlClientConfig_QuiltConfig_UIProvider implements ClientModInitializer {
+
+	private static final String MODID = "axolotlclientconfig-quilt-configconverter-test";
+
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+		Configs.getAll().forEach(c -> AxolotlClientConfigManager.getInstance().registerConfig(c.family(), new QuiltConfigConverter(c)));
+	}
+}
--- README.md
+++ README.md
@@ -15,5 +15,7 @@ public class Example implements ClientModInitializer {
 }
 ```
 
-You may also look at [AxolotlClientConfig_Quilt_Test.java](src/test/java/io/github/moehreag/AxolotlClientConfig_Quilt_Test.java)
-for further usage details (in this case by double-registering OKZoomer's configuration).
+You may also look at [AxolotlClientConfig_Quilt_Test.java](QuiltConfigUIProvider/src/main/java/io/github/moehreag/AxolotlClientConfig_QuiltConfig_UIProvider/AxolotlClientConfig_QuiltConfig_UIProvider.java)
+for further usage details.
+
+

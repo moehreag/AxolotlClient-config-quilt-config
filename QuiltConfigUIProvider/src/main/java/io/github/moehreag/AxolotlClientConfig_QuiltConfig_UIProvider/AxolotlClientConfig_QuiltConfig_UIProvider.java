cf043ca388bb6bbda5977fdedde6ce1b58c16346
moehreag <moehreag@gmail.com>
2023-03-11 17:08:29 +0100

    fix build

--- QuiltConfigUIProvider/src/main/java/io/github/moehreag/AxolotlClientConfig_QuiltConfig_UIProvider/AxolotlClientConfig_QuiltConfig_UIProvider.java
+++ QuiltConfigUIProvider/src/main/java/io/github/moehreag/AxolotlClientConfig_QuiltConfig_UIProvider/AxolotlClientConfig_QuiltConfig_UIProvider.java
@@ -1,6 +1,7 @@
 package io.github.moehreag.AxolotlClientConfig_QuiltConfig_UIProvider;
 
 import io.github.axolotlclient.AxolotlClientConfig.AxolotlClientConfigManager;
+import io.github.axolotlclient.AxolotlClientConfig.common.ConfigManager;
 import io.github.moehreag.AxolotlClientConfig_Quilt_Config.QuiltConfigConverter;
 import org.quiltmc.config.api.Configs;
 import org.quiltmc.loader.api.ModContainer;
@@ -8,10 +9,17 @@ import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 public class AxolotlClientConfig_QuiltConfig_UIProvider implements ClientModInitializer {
 
-	private static final String MODID = "axolotlclientconfig-quilt-configconverter-test";
-
 	@Override
 	public void onInitializeClient(ModContainer mod) {
-		Configs.getAll().forEach(c -> AxolotlClientConfigManager.getInstance().registerConfig(c.family(), new QuiltConfigConverter(c)));
+		Configs.getAll().forEach(c -> AxolotlClientConfigManager.getInstance().registerConfig(c.family(), new QuiltConfigConverter(c), new ConfigManager() {
+			@Override
+			public void save() {}
+
+			@Override
+			public void load() {}
+
+			@Override
+			public void loadDefaults() {}
+		}));
 	}
 }
